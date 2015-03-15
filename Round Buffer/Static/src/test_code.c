25e76654cde4ed551eac936d811b07623aaeedcf
KajtomatO <KajtomatO@KajtomatO_VIP>
2015-03-15 18:52:37 +0100

    Working#3

---
+++ Round Buffer/Static/src/test_code.c
@@ -0,0 +1,15 @@
+/*! \file
+ *  \brief File used to test library code.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*! \brief Function used to test library code.
+ *
+ * @return 0 if success, 1 if error.
+ */
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
